976d4bda35193ea99fb86cca02d579c5f5d0ef8c
Clearinx <horvath.tamas902@gmail.com>
2018-11-30 20:54:16 +0100

    final version for distance measurement

--- Documents/Arduino/DistanceMeasurement/DistanceMeasurement.ino
+++ Documents/Arduino/DistanceMeasurement/DistanceMeasurement.ino
@@ -1,8 +1,15 @@
 #include <elapsedMillis.h>
-#define SuperMaxValue 200
-#define MaxValue 200
-#define CompValue 200
-#define LowcompValue 145
+#include <MPU9250_asukiaaa.h>
+
+#ifdef _ESP32_HAL_I2C_H_
+#define SDA_PIN 26
+#define SCL_PIN 25
+#endif
+
+#define SuperMaxValue 225
+#define MaxValue 170
+#define CompValue 170
+#define LowcompValue 115
 #define MidValue 0
 #define LowValue 0
 
@@ -13,6 +20,8 @@
 
 #define swap(a,b) a ^= b; b ^= a; a ^= b;
 #define sort(a,b) if(a>b){ swap(a,b); }
+
+MPU9250 mySensor;
 
 int analogpin=8;
 float convert=55.0/1024.0;
@@ -34,11 +43,30 @@ int dir1PinB = 4;
 int dir2PinB = 5;
 int speedPinB = 10; // Needs to be a PWM pin to be able to control motor speed
 
+//////////////////////////
+int turnGrades = 39;
+/////////////////////////
 long frontRawUltrasData[5] = {0, 0, 0, 0, 0};
 long leftRawUltrasData[5] = {0, 0, 0, 0, 0};
 
-long frontFilteredUltrasData[5] = {0, 0, 0, 0, 0};
-long leftFilteredUltrasData[5] = {0, 0, 0, 0, 0};
+long frontFilteredUltrasData[5] = {3000, 3000, 3000, 3000, 3000};
+long leftFilteredUltrasData[5] = {3000, 3000, 3000, 3000, 3000};
+
+long firstStraightMeasured = 0;
+
+boolean debugStop = false;
+boolean stopped = false;
+boolean turnToRight = false;
+boolean firstStraight = false;
+boolean secondTurnToRight = false;
+boolean secondStraight = false;
+boolean lastCorrection = false;
+
+elapsedMillis turnTimer;
+int turnTimerValue;
+
+float originalDirection = 0;
+int incomingByte= 0;
 
 elapsedMillis TestTimer;
 
@@ -54,6 +82,20 @@ void setup(){
   pinMode(dir1PinB,OUTPUT);
   pinMode(dir2PinB,OUTPUT);
   pinMode(speedPinB,OUTPUT);
+
+  #ifdef _ESP32_HAL_I2C_H_ // For ESP32
+  Wire.begin(SDA_PIN, SCL_PIN); // SDA, SCL
+  #else
+  Wire.begin();
+  #endif
+  
+  mySensor.setWire(&Wire);
+  mySensor.beginMag();
+  mySensor.magYOffset = 200;
+
+  mySensor.magUpdate();
+  originalDirection = mySensor.magY();
+  
   Serial.println("Startup");
 }
 
@@ -131,12 +173,381 @@ void CollectLeftUltrasData(){
   leftFilteredUltrasData[0] = leftRawUltrasData[2];
 }
 
+void InitializeSpeedValues(){
+   int sensorValue=analogRead(analogpin);
+   int finalValue=sensorValue*convert;
+   
+   SuperMax=SuperMaxValue+finalValue;
+   Max=MaxValue+finalValue;
+   Comp=CompValue+finalValue;
+   Lowcomp=LowcompValue+finalValue;
+   Mid=MidValue+finalValue;
+   Low=LowValue+finalValue;
+}
 
+void Straight(boolean *toFalse, boolean *toTrue)
+{
+    Serial.println("Straight");
+    CollectLeftUltrasData();
+    int counterLow = 0;
+    int counterHigh = 0;
+    int counterOver = 0;
+    Serial.println(firstStraightMeasured);
+    for(int i=0; i<5; i++)
+    {
+      if(leftFilteredUltrasData[i] < firstStraightMeasured - 1){
+        counterLow++;
+      }
+      else if(leftFilteredUltrasData[i] > firstStraightMeasured + 1 && leftFilteredUltrasData[i] <= firstStraightMeasured + 25)
+      {
+        counterHigh++;
+      }
+      else if (leftFilteredUltrasData[i] > firstStraightMeasured + 25)
+      {
+        counterOver ++;
+      }
+    }
+    if(counterLow >= 3)
+    {
+      analogWrite(speedPinA, Max); //jobbra
+      analogWrite(speedPinB, 60);
+
+      digitalWrite(dir1PinA, LOW);
+      digitalWrite(dir2PinA, HIGH);
+      digitalWrite(dir1PinB, LOW);
+      digitalWrite(dir2PinB, HIGH);
+
+      delay(45);
+
+      analogWrite(speedPinA, Max); 
+      analogWrite(speedPinB, Max);
+
+      digitalWrite(dir1PinA, LOW);
+      digitalWrite(dir2PinA, HIGH);
+      digitalWrite(dir1PinB, LOW);
+      digitalWrite(dir2PinB, HIGH);
+
+      delay(5);
+      
+      Serial.println("Too close!");
+    }
+    else if (counterHigh >= 3)
+    {
+      analogWrite(speedPinA, 60); //jobbra
+      analogWrite(speedPinB, Max);
+
+      digitalWrite(dir1PinA, LOW);
+      digitalWrite(dir2PinA, HIGH);
+      digitalWrite(dir1PinB, LOW);
+      digitalWrite(dir2PinB, HIGH);
+
+      delay(45);
+
+      analogWrite(speedPinA, Max); 
+      analogWrite(speedPinB, Max);
+
+      digitalWrite(dir1PinA, LOW);
+      digitalWrite(dir2PinA, HIGH);
+      digitalWrite(dir1PinB, LOW);
+      digitalWrite(dir2PinB, HIGH);
+
+      delay(5);
+      Serial.println("Too far!");
+    }
+    else if(counterOver == 5)
+    {
+       Serial.println("Over");
+       *toFalse = false;
+       *toTrue = true;
+       delay(100);
+    }
+    else if (counterLow < 4 && counterHigh < 4)
+    {
+       analogWrite(speedPinA, Max); //előre
+       analogWrite(speedPinB, Max);
+       Serial.println("Just right!");
+    }
+    Serial.println(counterLow);
+    Serial.println(counterHigh);
+    Serial.println(counterOver);
+}
 
 void loop(){
+  incomingByte = Serial.read();
+  if(incomingByte != -1)
+  {
+      Serial.println(incomingByte);
+  }
+  if(incomingByte == 49)
+  {
+       stopped = true;
+       debugStop = true;
+  }
+  if(incomingByte == 50)
+  {
+       debugStop = false;
+       stopped = false;
+       turnToRight = false;
+       firstStraight = false;
+       secondTurnToRight = false;
+       secondStraight = false;
+       lastCorrection = false;
+  }
+  InitializeSpeedValues();
+  if(!stopped && !turnToRight && !debugStop && !firstStraight && !secondTurnToRight && !secondStraight && ! lastCorrection){
+    CollectFrontUltrasData();
+    int counter = 0;
+    
+    for(int i=0; i<5; i++)
+    {
+      if(frontFilteredUltrasData[i] < 40){
+        counter++;
+      }
+    }
 
-  //Serial.print("F: ");Serial.println(frontFilteredUltrasData[0]);
-  //Serial.print("L: ");Serial.println(L_distance);
-  
-  delay(1000);
+    if(counter < 4){
+      analogWrite(speedPinA, Max);
+      analogWrite(speedPinB, Max);
+      
+      digitalWrite(dir1PinA, LOW);
+      digitalWrite(dir2PinA, HIGH);
+    
+      digitalWrite(dir1PinB, LOW);
+      digitalWrite(dir2PinB, HIGH);
+    }
+    else
+    {
+      analogWrite(speedPinA, 0);
+      analogWrite(speedPinB, 0);
+      
+      digitalWrite(dir1PinA, LOW);
+      digitalWrite(dir2PinA, HIGH);
+    
+      digitalWrite(dir1PinB, LOW);
+      digitalWrite(dir2PinB, HIGH);
+      turnToRight = true;  
+      Serial.println("Turn right");
+    }
+    delay(100);
+  }
+  /*else if(turnToRight)
+  {
+    int counter = 0;
+    
+    analogWrite(speedPinA, SuperMax);
+    analogWrite(speedPinB, 0);
+    
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
+    turnTimer = 0;
+    do
+    {
+      CollectLeftUltrasData();
+      counter = 0;
+      for(int i=0; i<5; i++)
+      {
+        if(leftFilteredUltrasData[i] < 30){
+          counter++;
+        }
+        //Serial.println(leftFilteredUltrasData[i]);
+      }
+    }while(counter < 4);
+      analogWrite(speedPinA, 0);
+      analogWrite(speedPinB, 0);
+      
+      digitalWrite(dir1PinA, LOW);
+      digitalWrite(dir2PinA, HIGH);
+    
+      digitalWrite(dir1PinB, LOW);
+      digitalWrite(dir2PinB, HIGH);
+      turnToRight = false;
+      firstStraight = true;
+      CollectLeftUltrasData();
+      firstStraightMeasured = leftFilteredUltrasData[0];
+      turnTimerValue = turnTimer;
+      Serial.println("First straight");
+  }*/
+  else if(turnToRight)
+  {
+    mySensor.magUpdate();
+    float measured = mySensor.magY();
+
+    analogWrite(speedPinA, SuperMax);
+    analogWrite(speedPinB, 0);
+    
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
+    float value = 0;
+    do{
+        delay(10);
+        mySensor.magUpdate();
+        value = mySensor.magY();
+        Serial.print("M: ");Serial.println(measured);
+        Serial.println(value);
+        Serial.println(measured + turnGrades);
+    }while(value <= measured + turnGrades);
+
+    
+    analogWrite(speedPinA, 0);
+    analogWrite(speedPinB, 0);
+    
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
+    CollectLeftUltrasData();
+    firstStraightMeasured = leftFilteredUltrasData[0];
+    turnToRight= false;
+    firstStraight = true;
+    delay(100);
+  }
+  else if(firstStraight)
+  {
+    Straight(&firstStraight, &secondTurnToRight);
+    Serial.println("Second turn");
+  }
+  else if(secondTurnToRight)
+  {
+
+    analogWrite(speedPinA, 0);
+    analogWrite(speedPinB, 0);
+    
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
+    delay(100);
+    
+    analogWrite(speedPinA, 0);
+    analogWrite(speedPinB, SuperMax);
+    
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
+    float value = 0;
+    do{
+        mySensor.magUpdate();
+        value = mySensor.magY();
+        Serial.print("M: ");Serial.println(originalDirection);
+        Serial.println(value);
+    }while(value >= originalDirection+20);
+
+    
+    analogWrite(speedPinA, 0);
+    analogWrite(speedPinB, 0);
+    
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
+    firstStraightMeasured = leftFilteredUltrasData[0];
+    secondTurnToRight = false;
+    secondStraight = true;
+    delay(100);
+  }
+  /*else if(secondTurnToRight)
+  {
+    //Serial.println("SecondTurnToRight!");
+    analogWrite(speedPinA, 0);
+    analogWrite(speedPinB, SuperMax);
+    
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
+    turnTimer = 0;
+    do{
+      delay(1);
+      //Serial.println(turnTimer);
+      //Serial.println(turnTimerValue);
+    }while(turnTimer < turnTimerValue + 125);
+    secondTurnToRight = false;
+    secondStraight = true;
+    Serial.println("Second straight");
+  }*/
+  else if(secondStraight)
+  {
+      analogWrite(speedPinA, Max);
+      analogWrite(speedPinB, Max);
+      
+      digitalWrite(dir1PinA, LOW);
+      digitalWrite(dir2PinA, HIGH);
+      digitalWrite(dir1PinB, LOW);
+      digitalWrite(dir2PinB, HIGH);
+
+      delay(200);
+      int counter = 0;
+      do
+      {
+         analogWrite(speedPinA, Max); //jobbra
+         analogWrite(speedPinB, Max);
+
+         digitalWrite(dir1PinA, LOW);
+         digitalWrite(dir2PinA, HIGH);
+         digitalWrite(dir1PinB, LOW);
+         digitalWrite(dir2PinB, HIGH);
+
+         delay(10);
+         CollectLeftUltrasData();
+         counter = 0;
+         if(leftFilteredUltrasData[0] < 30){
+            counter++;
+         }
+         Serial.println("LookingForWall");
+      }while(counter < 1);
+      CollectLeftUltrasData();
+      firstStraightMeasured = leftFilteredUltrasData[0];
+
+
+      Serial.println("WallFound");
+      do
+      {
+         Straight(&secondStraight, &lastCorrection);
+      }while(secondStraight);
+  }
+  else if(lastCorrection)
+  {
+      
+    analogWrite(speedPinA, 0);
+    analogWrite(speedPinB, SuperMax);
+    
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
+    float value = 0;
+    do{
+        mySensor.magUpdate();
+        value = mySensor.magY();
+        Serial.print("M: ");Serial.println(originalDirection);
+        Serial.println(value);
+        Serial.println(value);
+    }while(value >= originalDirection);
+
+    
+    analogWrite(speedPinA, 0);
+    analogWrite(speedPinB, 0);
+    
+    digitalWrite(dir1PinA, LOW);
+    digitalWrite(dir2PinA, HIGH);
+    digitalWrite(dir1PinB, LOW);
+    digitalWrite(dir2PinB, HIGH);
+    firstStraightMeasured = leftFilteredUltrasData[0];
+    lastCorrection = false;
+    stopped = true;
+    delay(100);
+  }
+  else if(debugStop)
+  {
+        mySensor.magUpdate();
+      float measured = mySensor.magY();
+      Serial.println(measured);
+      delay(500);
+  }
+
 }
